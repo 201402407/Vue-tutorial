2fa4caf55d852132211f873d136428626b1ab625
201402407 <banner4@naver.com>
2021-04-13 16:48:15 +0900

    이게 맞음

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
+import Test from "@/views/test/index.vue";
+import Main from "@/views/main/index.vue";
 
 Vue.use(VueRouter);
 
@@ -11,19 +13,20 @@ const routes: Array<RouteConfig> = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/test",
+    name: "Test",
+    component: Test,
   },
+  {
+    path: "/main",
+    name: "Main",
+    component: Main,
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  base: "/spa",
   routes,
 });
 
