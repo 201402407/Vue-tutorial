904e1b8cbd52a406a35e74848c3d8e27113b32ec
201402407 <banner4@naver.com>
2021-04-13 16:23:13 +0900

    소스 추가

---
+++ src/views/main/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>메인페이지</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'vue-property-decorator'
+
+@Component
+export default class Test extends Vue {
+  created() { // init 후 DOM 추가 전 호출. DOM에 컴포넌트가 MOUNT 되기 전. $el 사용 X
+    console.log("Created!");
+  }
+
+  mounted() { // 컴포넌트가 DOM에 추가된 후 호출.
+    console.log("Mounted!");
+    this.$nextTick(() => {
+      // 모든 화면이 렌더링 된 후 호출
+      console.log("모든 화면 렌더링 완료!");
+    })
+  }
+}
+
+</script>
---
+++ src/views/test/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="test">{{ count }} <br>
+        <button @click="onClick('2')">카운트증가</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'vue-property-decorator'
+
+@Component
+export default class Test extends Vue {
+    private count = 1
+    
+    @Emit('click')
+    onClick(num: number) {
+        console.log("onClick");
+        return this.count + num
+    }
+}
+
+</script>
