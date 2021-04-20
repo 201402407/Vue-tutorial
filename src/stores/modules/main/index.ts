0f0c75172e6011f79812e654481bd944a816eb0e
201402407 <banner4@naver.com>
2021-04-21 02:16:48 +0900

    Axios REST API 하나 생성:

--- src/stores/modules/main/index.ts
+++ src/stores/modules/main/index.ts
@@ -1,35 +1,48 @@
-import store from '@/stores/index'
-import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
-import RestSample, { ItemList, ResVo } from '@/services/main/restSample'
-import Vue from 'vue'
+import store from "@/stores/index"
+import {
+  getModule,
+  Module,
+  VuexModule,
+  Mutation,
+  Action,
+} from "vuex-module-decorators"
+import RestSample, { ItemList, ResVo } from "@/services/main/restSample"
+import Vue from "vue"
 
-@Module({ dynamic: true, name: 'MainModule', namespaced: true, store })
-export default class MainModule extends VuexModule {
-    // 클래스 내부 변수 선언
-    itemList!: ItemList
-    resVo!: ResVo
-
-    // itemList  = [
-    //     { id: 1, item: '씻기', status: 'clear' },
-    //     { id: 2, item: '준비하기', status: 'clear' },
-    //     { id: 3, item: '학교가기', status: 'clear' },
-    //     { id: 4, item: '게임하기', status: 'yet' },
-    // ]
-
-    // 서비스 또는 작업 수행(서버 연동)
-    @Action
-    async reqMainInfo() {
-        // await new RestSample().getMainInfo()    // 여긴 서버 API 통신 함수 호출
-    }
-
-    // Data Store에 저장하는 Annotation
-    @Mutation
-    setMainInfos() {
-        Vue.set(this, 'itemList', [])
-        Vue.set(this, 'resVo', {})
-    }
-
-    get getResVo() {
-        return this.resVo
-    }
+export interface LoginPVO {
+  id: string
+  pwd: string
 }
+
+@Module({ dynamic: true, name: "mainStore", namespaced: true, store })
+export default class MainStore extends VuexModule {
+  // 클래스 내부 변수 선언
+  itemList!: ItemList
+  resVo!: ResVo
+
+  // itemList  = [
+  //     { id: 1, item: '씻기', status: 'clear' },
+  //     { id: 2, item: '준비하기', status: 'clear' },
+  //     { id: 3, item: '학교가기', status: 'clear' },
+  //     { id: 4, item: '게임하기', status: 'yet' },
+  // ]
+
+  // 서비스 또는 작업 수행(서버 연동)
+  @Action
+  async chkLogin(pvo: LoginPVO) {
+    await new RestSample().login(pvo) // 여긴 서버 API 통신 함수 호출
+  }
+
+  // Data Store에 저장하는 Annotation
+  @Mutation
+  setMainInfos() {
+    Vue.set(this, "itemList", [])
+    Vue.set(this, "resVo", {})
+  }
+
+  get getResVo() {
+    return this.resVo
+  }
+}
+
+export const MainModule = getModule(MainStore)
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,32 +1,48 @@
 <template>
-    <div><h1>메인페이지</h1><br>
-    <button @click='getStoreNum'>스토어 값 확인하기</button><br><br>
-    {{ testNum }} <br>
-    <button @click='getStoreName'>스토어 이름 확인하기</button><br><br>
+  <div>
+    <h1>메인페이지</h1>
+    <br />
+    <button @click="getStoreNum">스토어 값 확인하기</button><br /><br />
+    {{ testNum }} <br />
+    <button @click="getStoreName">스토어 이름 확인하기</button><br /><br />
     {{ testName }}
-    </div>
-    
+    <button @click="postLogin">Login API 통신</button><br /><br />
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Emit } from 'vue-property-decorator'
-import { TestModule } from '@/stores/modules/test/index'
+import { Vue, Component, Emit } from "vue-property-decorator"
+import { TestModule } from "@/stores/modules/test/index"
+import { MainModule } from "@/stores/modules/main/index"
+import { LoginPVO } from "@/stores/modules/main/index"
 
 @Component
-export default class Test extends Vue {
+export default class Main extends Vue {
   private testNum = 0
-  private testName = ''
+  private testName = ""
 
-  created() { // init 후 DOM 추가 전 호출. DOM에 컴포넌트가 MOUNT 되기 전. $el 사용 X
-    console.log("Created!");
+  private pvo: LoginPVO = {
+    id: "test",
+    pwd: "test",
   }
 
-  mounted() { // 컴포넌트가 DOM에 추가된 후 호출.
-    console.log("Mounted!");
+  created() {
+    // init 후 DOM 추가 전 호출. DOM에 컴포넌트가 MOUNT 되기 전. $el 사용 X
+    console.log("Created!")
+  }
+
+  mounted() {
+    // 컴포넌트가 DOM에 추가된 후 호출.
+    console.log("Mounted!")
     this.$nextTick(() => {
       // 모든 화면이 렌더링 된 후 호출
-      console.log("모든 화면 렌더링 완료!");
+      console.log("모든 화면 렌더링 완료!")
     })
+  }
+
+  postLogin() {
+    const rvo = MainModule.chkLogin(this.pvo)
+    console.log(rvo)
   }
 
   getStoreNum() {
@@ -38,5 +54,4 @@ export default class Test extends Vue {
     this.testName = TestModule.getName
   }
 }
-
 </script>
